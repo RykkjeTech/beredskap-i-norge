8da380b91122e34d4d8217205cff47f9d6e03361
KristianRykkje <kriryk@gmail.com>
2023-07-12 11:01:58 +0200

    better description

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Beredskap i Norge",
-  description: "Beredskap i Norge",
+  description:
+    "Offentlige tilfluktsrom i Norge. Tilfluktsrom er permanente beskyttelsesrom som skal verne befolkningen mot skader ved krigshandlinger. Offentlige tilfluktsrom er for befolkningen i et område og er bygget i byer og større tettsteder, samt i boligområder hvor dekningen av private tilfluktsrom ikke er tilfredsstillende.",
 };
 
 export default async function RootLayout({
