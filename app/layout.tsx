aae1b6d9e163b30bdcdb605db89be3abd6c56fa7
KristianRykkje <kriryk@gmail.com>
2023-07-12 11:15:01 +0200

    added authors

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ export const metadata: Metadata = {
   title: "Beredskap i Norge",
   description:
     "Offentlige tilfluktsrom i Norge. Tilfluktsrom er permanente beskyttelsesrom som skal verne befolkningen mot skader ved krigshandlinger. Offentlige tilfluktsrom er for befolkningen i et område og er bygget i byer og større tettsteder, samt i boligområder hvor dekningen av private tilfluktsrom ikke er tilfredsstillende.",
+  authors: [
+    {
+      name: "Rykkje Technologies Team",
+      url: "https://www.rykkje.tech",
+    },
+  ],
+  publisher: "Rykkje Technologies AS",
 };
 
 export default async function RootLayout({
